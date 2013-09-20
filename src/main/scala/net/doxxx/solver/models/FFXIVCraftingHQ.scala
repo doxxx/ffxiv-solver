be3dc729e12716af0f693b3c5b04bede828b0885
Gordon Tyler <gordon@doxxx.net>
2013-09-19 21:09:51 -0400

    Model effective progress and quality increase.

--- src/main/scala/net/doxxx/solver/models/FFXIVCraftingHQ.scala
+++ src/main/scala/net/doxxx/solver/models/FFXIVCraftingHQ.scala
@@ -8,8 +8,6 @@ class FFXIVCraftingHQ(charLevel: Int,
                       recipeLevel: Int,
                       baseCraftsmanship: Int,
                       baseControl: Int,
-                      baseProgressIncrease: Int,
-                      baseQualityIncrease: Int,
                       startDurability: Int,
                       startCP: Int,
                       startQuality: Int,
@@ -20,16 +18,16 @@ class FFXIVCraftingHQ(charLevel: Int,
                      durabilityCost: Int,
                      cpCost: Int,
                      successRate: Double,
-                     qualityIncrease: Int,
-                     progressIncrease: Int)
+                     qualityEfficiency: Double,
+                     progressEfficiency: Double)
 
   val NoAction = Action("NOP", 0, 0, 0, 0, 0)
-  val BasicSynth = Action("BS", 10, 0, 0.9, 0, baseProgressIncrease)
-  val BasicTouch = Action("BT", 10, 18, 0.7, baseQualityIncrease, 0)
+  val BasicSynth = Action("BS", 10, 0, 0.9, 0, 1)
+  val BasicTouch = Action("BT", 10, 18, 0.7, 1, 0)
   val MastersMend = Action("MM", -30, 94, 1, 0, 0)
   val InnerQuiet = Action("IQ", 0, 18, 1, 0, 0) // toggle, quality increases will increase control
   val SteadyHand = Action("SH", 0, 22, 1, 0, 0) // Improves action success rate by 20% for the next five steps
-  val HastyTouch = Action("HT", 10, 0, 0.5, baseQualityIncrease, 0)
+  val HastyTouch = Action("HT", 10, 0, 0.5, 0, 0)
   val WasteNot = Action("WN", 0, 56, 1, 0, 0) // Reduces loss of durability by 50% for the next four steps
 
   val actions = IndexedSeq(
@@ -39,6 +37,53 @@ class FFXIVCraftingHQ(charLevel: Int,
 
   def specimenBuilder(actions: Iterable[Action]): Vector[Action] = actions.toVector
 
+  /*
+  Level.Difference = Crafter.Level - Synth.Level
+
+  For -5 <= Level.Difference < 0,
+  Level.Correction.Factor = 0.10 * Level.Difference
+  For 0 < Level.Difference <= 5,
+  Level.Correction.Factor = 0.05 * Level.Difference
+  For 5 < Level.Difference <=15,
+  Level.Correction.Factor = (0.022 * Level.Difference) + 0.15
+
+  Base.Progress = 0.21 * (Craftsmanship) + 1.6
+  Level.Corrected.Progress = Base.Progress * (1 + Level.Correction.Factor)
+
+
+
+  For -5 <= Level.Difference <= 0,
+  Level.Correction.Factor = 0.05 * Level.Difference
+
+  Base.Quality = 0.36 * (Craftsmanship) + 34
+  Level.Corrected.Quality = Base.Quality * (1 + Level.Correction.Factor)
+
+  */
+
+  def effectiveProgressIncrease(craftsmanship: Double): Int = {
+    val levelDiff = charLevel - recipeLevel
+    val correctionFactor = {
+      if (levelDiff >= -5 && levelDiff <= 0) 0.10 * levelDiff
+      else if (levelDiff > 0 && levelDiff <= 5) 0.05 * levelDiff
+      else if (levelDiff > 5) 0.022 * levelDiff + 0.15
+      else 0
+    }
+    val baseProgress = 0.21 * craftsmanship + 1.6
+
+    math.round(baseProgress * (1 + correctionFactor)).toInt
+  }
+
+  def effectiveQualityIncrease(control: Double): Int = {
+    val levelDiff = charLevel - recipeLevel
+    val correctionFactor = {
+      if (levelDiff >= -5 && levelDiff <= 0) 0.05 * levelDiff
+      else 0
+    }
+    val baseQuality = 0.36 * control + 34
+
+    math.round(baseQuality * (1 + correctionFactor)).toInt
+  }
+
   case class State(durability: Int, cp: Int, quality: Double, progress: Double, steadyHand: Int,
                     craftsmanship: Double, control: Double) {
     def apply(action: Action) = {
@@ -46,8 +91,8 @@ class FFXIVCraftingHQ(charLevel: Int,
       copy(
         durability - action.durabilityCost,
         cp - action.cpCost,
-        quality + action.qualityIncrease * successRate,
-        progress - action.progressIncrease * successRate,
+        quality + effectiveQualityIncrease(control) * action.qualityEfficiency * successRate,
+        progress - effectiveProgressIncrease(craftsmanship) * action.progressEfficiency * successRate,
         steadyHand = if (action == SteadyHand) 5 else math.max(0, steadyHand - 1),
         craftsmanship = craftsmanship,
         control = control
@@ -122,18 +167,16 @@ object FFXIVCraftingHQ extends App {
   // BS SH BT BT BT BT MM BS BS => ~234
   // BS SH HT HT BT MM SH HT BT HT BS BS => 299
   // SH BT BT HT BT HT MM BT BS BS BS => 312
-  val archetype = "NOP NOP NOP NOP NOP SH BT BT HT BT HT MM BT BS BS BS".split(' ').toVector
+  val archetype = "NOP NOP NOP NOP NOP BS BS BS MM HT SH BT BT BT BT BS".split(' ').toVector
   val model = new FFXIVCraftingHQ(
     charLevel = 12,
     recipeLevel = 12,
-    baseCraftsmanship = 10,
-    baseControl = 10,
-    baseProgressIncrease = 26,
-    baseQualityIncrease = 65,
+    baseCraftsmanship = 83,
+    baseControl = 86,
     startDurability = 60,
     startCP = 190,
     startQuality = 0,
-    difficulty = 55,
+    difficulty = 53,
     archetype = archetype
   )
   model.run()
